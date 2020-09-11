97053aee9329f262da957d504597227f5523d772
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-09-11 16:52:04 +0800

    Add examples to show how ArrayList works

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -7,6 +7,9 @@ import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Circus {
     private static Animal[] animals = {
             new Duck("Drake"),
@@ -40,8 +43,34 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        makeAnimalsTalk();
-        System.out.println("Total value of equipments " + calculateValue(equipments));
-        System.out.println("Total value of animals " + calculateValue(animals));
+//        makeAnimalsTalk();
+//        System.out.println("Total value of equipments " + calculateValue(equipments));
+//        System.out.println("Total value of animals " + calculateValue(animals));
+
+//        animals[2] = new Duck("Louie");
+//        System.out.println(animals.length);
+
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+
+        animalArrayList.add(new Duck("Goose"));
+        animalArrayList.add(new Parrot("Dolly"));
+
+        Duck louie = new Duck("Louie");
+        animalArrayList.add(louie);
+
+        printAnimals(animalArrayList);
+        System.out.println("Number of animals: " + animalArrayList.size());
+        System.out.println("Index of Louie: " + animalArrayList.indexOf(louie));
+
+        animalArrayList.sort(Animal.AnimalNameComparator);
+        printAnimals(animalArrayList);
+        animalArrayList.remove(louie);
+        System.out.println("Index of Louie: " + animalArrayList.indexOf(louie));
+    }
+
+    private static void printAnimals(ArrayList<Animal> animalArrayList) {
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
     }
 }
